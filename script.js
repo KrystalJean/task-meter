a3145b13cc7059798367c9d7e80abe0014a7922b
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2022-11-30 18:28:14 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const meter = document.getElementById('meter');
+const emotions = document.getElementById('emotions');
+const calendar = document.getElementById('calendar');
+const choices = document.getElementById('choices');
+
+const tasksPerDay = 4;
+
