d18336190f73266314acefb6c15ba05dc39820ff
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2022-11-30 19:53:49 -0600

    meter fills according to length of task list

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const meter = document.getElementById('meter');
-const emotions = document.getElementById('emotions');
-const calendar = document.getElementById('calendar');
-const choices = document.getElementById('choices');
+const meterEmpty = document.getElementById('meter-empty');
+const meterFill = document.getElementById('meter-fill');
+const emoCon = document.getElementById('emotions');
+const calCon = document.getElementById('calendar');
+const choiCon = document.getElementById('choices');
+const choice1 = document.getElementById('choice1');
 
 const tasksPerDay = 4;
+const todaysTasks = [0,1,2,3,4,5,6];
+
+let fillAmount = meterEmpty.offsetHeight/todaysTasks.length;
+
+
+meterFill.style.height = fillAmount +'px';
+
+
+
+
+
+
 
