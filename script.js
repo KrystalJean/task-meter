47a4f06430cee9e5bb4af10a819f1126e351a15e
kaustin <krystalaustin99@gmail.com>
2022-12-03 15:00:17 -0600

    click task in choices to clone them to my tasks

--- script.js
+++ script.js
@@ -2,16 +2,40 @@ const meterEmpty = document.getElementById('meter-empty');
 const meterFill = document.getElementById('meter-fill');
 const emoCon = document.getElementById('emotions');
 const calCon = document.getElementById('calendar');
-const choiCon = document.getElementById('choices');
-const choice1 = document.getElementById('choice1');
 
-const tasksPerDay = 4;
-const todaysTasks = [0,1,2,3,4,5,6];
+const choiceCon = document.getElementById('choices');
+const tasks = document.querySelectorAll('.tasks');
+
+
+
+
+// const tasksPerDay = 4;
+const todaysTasks = [];
 
 let fillAmount = meterEmpty.offsetHeight/todaysTasks.length;
 
 
 meterFill.style.height = fillAmount +'px';
+
+tasks.forEach(task => {
+    task.addEventListener('click', () => {
+       
+        const bigTask = task.cloneNode(true);
+        bigTask.classList.add('big-tasks');
+        meterEmpty.append(bigTask);
+        setTimeout(removeClass, 3000);
+        function removeClass() {
+            bigTask.classList.remove('tasks');
+        }
+        
+        
+
+        // const littleTask = task.cloneNode(true);
+        
+    })
+});
+
+
 
 
 
